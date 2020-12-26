ab5771112a20ccf8f3eb5c91b3e3077a42452e44
Piotr Ładoński <piolado@gmail.com>
2020-12-26 02:04:16 +0100

    Messy-but-working fiat exchange PoC

--- currency-exchange/src/components/HelloWorld.vue
+++ currency-exchange/src/components/HelloWorld.vue
@@ -1,27 +1,49 @@
 <template>
-  <button @click="increment">
-    Count is: {{ state.count }}, double is: {{ state.double }}
-  </button>
+  <input type="number" placeholder="Source value" v-model="sourceValue" />
+  <input type="text" placeholder="Source currency" v-model="sourceCurrency" />
+  <input type="text" placeholder="Target currency" v-model="targetCurrency" />
+  <br />
+  <p>sourceValue is: {{ sourceValue }}</p>
+  <p>sourceCurrency is: {{ sourceCurrency }}</p>
+  <p>targetCurrency is: {{ targetCurrency }}</p>
+  <br />
+  <p>result: {{ resultValue }}</p>
+  <br />
+  <button @click="convert">convert</button>
+  <p>Currencies: {{ currencies }}</p>
 </template>
 
 <script>
-import { reactive, computed } from "vue";
+import { ref } from "vue";
+import { useFiatExchangeApi } from "../composables/fiatExchangeApi";
 
 export default {
   setup() {
-    const state = reactive({
-      count: 0,
-      double: computed(() => state.count * 2)
-    });
+    const sourceCurrency = ref("");
+    const targetCurrency = ref("");
+    const sourceValue = ref(0);
+    const resultValue = ref(0);
 
-    function increment() {
-      state.count++;
-    }
+    const { fiatCurrencies, fiatConvert } = useFiatExchangeApi();
+
+    const convert = async () => {
+      resultValue.value = await fiatConvert(
+        sourceCurrency.value,
+        targetCurrency.value,
+        sourceValue.value
+      );
+    };
 
     return {
-      state,
-      increment
+      sourceCurrency,
+      targetCurrency,
+      sourceValue,
+      resultValue,
+      currencies: fiatCurrencies,
+      convert
     };
   }
 };
 </script>
+
+<style scoped lang="scss"></style>
--- currency-exchange/src/main.js
+++ currency-exchange/src/main.js
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import runtime from "serviceworker-webpack-plugin/lib/runtime";
 
-if ("serviceWorker" in navigator) {
+const useSw = false;
+if ("serviceWorker" in navigator && useSw) {
   runtime.register();
+} else {
+  console.log("Service worker has been disabled in main.js");
 }
 
 createApp(App).mount("#app");
