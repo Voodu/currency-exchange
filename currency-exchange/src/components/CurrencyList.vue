54a53c37746a931ac5e37313a0ceadf0118874a8
Piotr Ładoński <piolado@gmail.com>
2020-12-26 22:30:09 +0100

    Add currency list component

---
+++ currency-exchange/src/components/CurrencyList.vue
@@ -0,0 +1,30 @@
+<template>
+  <select
+    :value="modelValue"
+    @change="$emit('update:modelValue', $event.target.value)"
+  >
+    <option
+      v-for="currency in sortedCurrencies"
+      :key="currency"
+      :value="currency"
+    >
+      {{ currency }}
+    </option>
+  </select>
+  <!-- Purely magical comment -->
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  props: { modelValue: String, currencies: Array },
+  emits: ["update:modelValue"],
+  setup(props) {
+    const sortedCurrencies = computed(() => [...props.currencies].sort());
+    return { sortedCurrencies };
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
