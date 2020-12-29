ed0260c11cd9635ad6c10e64ae9f47a471c13bf4
Piotr Ładoński <piolado@gmail.com>
2020-12-29 21:01:01 +0100

    Improve main page visuals

--- currency-exchange/src/App.vue
+++ currency-exchange/src/App.vue
@@ -1,5 +1,16 @@
 <template>
+  <div class="w-100 align-self-center">
+    <h1 class="m-0 font-weight-bold text-center">Currency Exchanger</h1>
+  </div>
   <CurrencyExchanger />
+  <footer class="w-100 align-self-end">
+    <small class="m-0 font-italic text-left">
+      Note: Don't plan your financial future using this app. It caches exchange
+      rates, so they are not as up to date as they should be. Also, some
+      exchange rates are calculated using others, so they are prone to rounding
+      errors.
+    </small>
+  </footer>
 </template>
 
 <script>
@@ -15,17 +26,15 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   height: 100vh;
+  max-width: 800px;
   padding: 2rem;
+  margin: auto;
 
   // Vertical center children in itself
   align-items: center;
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
 }
 </style>
