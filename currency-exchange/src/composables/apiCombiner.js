05f817dee3c0ae419bf963a8d444cf16956224ba
Piotr Ładoński <piolado@gmail.com>
2020-12-29 15:36:11 +0100

    Add conversion between combined apis

--- currency-exchange/src/composables/apiCombiner.js
+++ currency-exchange/src/composables/apiCombiner.js
@@ -2,23 +2,57 @@ import { ref, watch } from "vue";
 import { useCryptoExchangeApi } from "../composables/cryptoExchangeApi";
 import { useFiatExchangeApi } from "../composables/fiatExchangeApi";
 
+const BASE = "EUR";
+
 export function useApiCombiner() {
   const apis = [useCryptoExchangeApi(), useFiatExchangeApi()];
-  //   const exchangeRates = ref({});
   const currencies = ref([]);
 
   for (const api of apis) {
     watchAndCombineCurrencies(api, currencies);
   }
 
-  const convert = (sourceCurrency, targetCurrency, value) => {
-    console.log(sourceCurrency, targetCurrency, value);
+  const convert = (srcCurrency, dstCurrency, value) => {
+    let srcApi = getApiWithCurrency(apis, srcCurrency);
+    let dstApi = getApiWithCurrency(apis, dstCurrency);
+    validateApis(srcApi, dstApi);
+
+    const toCommon = srcApi.convert(srcCurrency, BASE, value);
+    const fromCommon = dstApi.convert(BASE, dstCurrency, toCommon);
+    return fromCommon;
   };
 
   return {
     currencies,
     convert
   };
+}
+
+function validateApis(srcApi, dstApi) {
+  if (srcApi == null || dstApi == null) {
+    throw new Error(
+      "Could not find API with either source or target currency."
+    );
+  }
+  if (!isBaseInBothApis(srcApi, dstApi)) {
+    throw new Error(`Common base (${BASE}) not found in one of the APIs.`);
+  }
+}
+
+function isBaseInBothApis(srcApi, dstApi) {
+  return (
+    srcApi.currencies.value.includes(BASE) &&
+    dstApi.currencies.value.includes(BASE)
+  );
+}
+
+function getApiWithCurrency(apis, srcCurrency) {
+  for (const api of apis) {
+    if (api.currencies.value.includes(srcCurrency)) {
+      return api;
+    }
+  }
+  return null;
 }
 
 function watchAndCombineCurrencies(api, allCurrencies) {
