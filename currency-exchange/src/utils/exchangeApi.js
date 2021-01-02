4360de8a696d7aec7f0c4241a1bcc5a7d4e7f108
Piotr Ładoński <piolado@gmail.com>
2021-01-02 17:16:06 +0100

    Add missing prop names in exchange api class

--- currency-exchange/src/utils/exchangeApi.js
+++ currency-exchange/src/utils/exchangeApi.js
@@ -1,8 +1,8 @@
 /**
  * @class ExchangeApi
  * @type {Object}
- * @property {Array.<string>} Currencies available in the API
- * @property {function(string, string, number)} API conversion function
+ * @property {Array.<string>} currencies Currencies available in the API
+ * @property {function(string, string, number)} convert API conversion function
  */
 export class ExchangeApi {
   constructor(currencies, convert) {
